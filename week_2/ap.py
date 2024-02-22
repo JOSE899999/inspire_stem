aa24d5e95ae630527cde0aa4941ca8a6e4eb1c0b
unknown <thuoj6980@gmail.com>
2024-02-22 08:46:13 +0300

    first commit

---
+++ week_2/ap.py
@@ -0,0 +1,16 @@
+# This is a program to do arithmetic progression
+# Date :20/2/2024 
+# Name : Ephraim
+
+a = float(input ("Enter the constant"))
+n = float(input ("Enter the term "))
+d = float(input ("Enter the difference"))
+
+b =(a +(n-1) * d)
+
+g_1 =(b)
+
+print("The solution of the ap:",g_1)
+
+
+
---
+++ week_2/gp.py
@@ -0,0 +1,11 @@
+#This is a program to calculate the geometric progression 
+#Date : 20/2/2024
+#Name :Ephraim
+
+a = float(input("Enter the constant  "))
+r = float(input("Enter the factor : "))
+n = float(input ("Enter the term : "))
+
+b = (a * r ** (n-1))
+
+print("the solution of the gp ",b)
