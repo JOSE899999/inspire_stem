fcaf5f70a04d320e12bdb5170df91ceb4a481bfb
JOSE899999 <thuoj6980@gmail.com>
2024-02-22 10:23:48 +0300

    first commit

---
+++ week_2/loops.py
@@ -0,0 +1,32 @@
+#loops and iterations
+#Date
+#Name
+
+
+#for loop
+#while loop
+#
+
+for number in range(0,10):
+    print(number)
+    print("\t")
+
+    #squares of the number
+
+    for x in range(0,11):
+        print("\t")
+        print(x**2)#squares
+
+for y in range(0,11):
+    print(str(y**2)+ "\t",end="")
+for x in range(0,11):
+    print(str(x**2)+ "\t",end="")
+
+for x in range(0,11):
+    print(str(x**2)+ "\t",end="")
+for y in range(1,11):
+    print(str(y**2)+"\t",end="")
+
+
+        
+
---
+++ week_2/strings.py
@@ -0,0 +1,64 @@
+#strings in python
+#Date
+#Name:JOseph
+
+city="nairobi"
+
+#convert to uppercase
+print(city)
+print("\n")
+print(city.upper())
+name="JOSEPH THUO"
+print(name)
+print(name.lower)#converts to lowercase
+
+town="   Naivasha"
+
+print(town)
+print("\t")#new tab
+print(town.strip())
+
+f_name="Joseph"
+s_name="Thuo"
+full_name=f_name+s_name
+
+print(full_name)
+
+print(city[0])
+print(city[-1])
+print(city[-2])
+print(city[-3])
+print(city[-4])
+print(city[-5])
+print(city[-6])
+
+pull="push"
+print(pull[-3])
+print(pull[-2])
+print(pull[-1])
+print(pull[0])
+
+
+#replacing a character
+fruit="apple"
+
+print(fruit.replace("a","b"))
+
+subject="chemistry:maths"
+
+print(subject.split(":"))
+
+age=30
+print("I am {} years old".format(age))
+height=183
+print("I am {0} years old and height {1} cm".format(age,height))
+
+
+
+
+
+
+
+
+
+
